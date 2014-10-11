c23d7b4eaed10594737dbd55f16936eb42b2505c
javier23 <javier23@javier23-PC>
2014-10-11 17:37:04 +0200

    ejerciicos de programacion da unidade 1

---
+++ ProgramacionFP/src/programacionfp/unidad2/Ejercicio8.java
@@ -0,0 +1,17 @@
+package programacionfp.unidad2;
+public class Ejercicio8 {
+     public static void main(String args[]){
+
+       int a=10;
+       System.out.println(a);
+       a/=2;
+       System.out.println(a);
+       a+=3;
+       System.out.println(a);
+       a*=2;
+       System.out.println(a);
+       a-=2;
+       System.out.println(a);
+
+   }
+}
---
+++ ProgramacionFP/src/programacionfp/unidad2/Ejercicio9.java
@@ -0,0 +1,7 @@
+package programacionfp.unidad2;
+
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        System.out.println(" las comillas dobles son asi \" y las simples asi \' ");
+    }
+}
