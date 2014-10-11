e95d724c286d32b85521438cf76ae238070b3e8a
javier23 <javier23@javier23-PC>
2014-10-11 13:04:17 +0200

    tres

--- ProgramacionFP/src/programacionfp/ProgramacionFP.java
+++ ProgramacionFP/src/programacionfp/ProgramacionFP.java
@@ -15,7 +15,7 @@ public class ProgramacionFP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("ahora vay desde otroo lado");
+        System.out.println("xa funciona");
     }
     
 }
