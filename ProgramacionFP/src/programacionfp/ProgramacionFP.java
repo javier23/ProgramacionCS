fe48df50e28bb79ebb8871895617ec9d47992d0f
Usuario <Usuario@javier23>
2014-10-11 13:09:08 +0200

    cuatro

--- ProgramacionFP/src/programacionfp/ProgramacionFP.java
+++ ProgramacionFP/src/programacionfp/ProgramacionFP.java
@@ -15,7 +15,7 @@ public class ProgramacionFP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("xa funciona");
+        System.out.println("xa funciona e funciona ben ao demas");
     }
     
 }
