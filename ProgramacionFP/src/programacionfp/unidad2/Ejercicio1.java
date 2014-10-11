d579574ba6523b5f2d0ad5dd8c3c4bcb46353dde
javier23 <javier23@javier23-PC>
2014-10-11 17:25:03 +0200

    ejerciicos de programacion da unidade 1

--- ProgramacionFP/src/programacionfp/unidad2/Ejercicio1.java
+++ ProgramacionFP/src/programacionfp/unidad2/Ejercicio1.java
@@ -2,15 +2,15 @@ package programacionfp.unidad2;
 public class Ejercicio1 {
     public static void main(String[] args) {
         // ua (Unidad Astronómica) = 149 597 870 km
-        double ua = 149597870d;
+        final double  UA = 149597870d;
         double distanciaSolPluton = 39.44;
-        double kmSolPluton = distanciaSolPluton * ua;
+        double kmSolPluton = distanciaSolPluton * UA;
         System.out.println("La distancia entre el Sol y Plutón es: " + kmSolPluton + " ua");
         
         
-        float ua2 = 149597870f;
+        float UA2 = 149597870f;
         float distanciaSolPluton2 = 39.44f;
-        float kmSolPluton2 = distanciaSolPluton2 * ua2;
+        float kmSolPluton2 = distanciaSolPluton2 * UA2;
         System.out.println("La distancia entre el Sol y Plutón es: " + kmSolPluton2 + " ua");
     }
 }
--- ProgramacionFP/src/programacionfp/unidad2/Ejercicio7.java
+++ ProgramacionFP/src/programacionfp/unidad2/Ejercicio7.java
@@ -1,9 +1,5 @@
 package programacionfp.unidad2;
+
 public class Ejercicio7 {
-    public static void main(String[] args) {
-        int a =32;
-        int b = 2;
-        int c = 32 >> 2;
-        System.out.println("resultado division: " + c);
-    }
+
 }
